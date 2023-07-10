5719211e4a0c47ba7c3834d8c1dee1440138d0ca
jexalto99 <48797422+jexalto@users.noreply.github.com>
2023-07-10 18:28:50 +0200

    pipelines|test pipelines

--- src/tests/test_dataclasses.py
+++ src/tests/test_dataclasses.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import sys, os
 import unittest
 
+# sys.path.append(os.path.join(Path(__file__).parents[2]))
+
 # --- Internal ---
 from src.base import DroneInfo, GridInfo
 
