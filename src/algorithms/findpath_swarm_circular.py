7705a163cfb593ff6fc6e8028a80b9b34920ba95
jexalto99 <48797422+jexalto@users.noreply.github.com>
2023-07-17 15:11:37 +0200

    algo|swarm algo seems to work

--- src/algorithms/findpath_swarm_circular.py
+++ src/algorithms/findpath_swarm_circular.py
@@ -21,20 +21,7 @@ class FindPathSwarm(FindPathGreedyTwoLayers):
         # === Initialise drones on grid and set starting point grid value to zero ===
         self._initialise(dronegrid_properties=dronegrid_properties)
         self.nr_drones = len(dronegrid_properties)
-        
-    def _initialise(self, dronegrid_properties: list[DroneGridInfo])->None:
-        starting_x, starting_y = dronegrid_properties[0].drone.starting_point
-        for idronegrid in dronegrid_properties:
-            idronegrid.grid.reset()
-            # When initialising the dorne on the grid the multiplier should be set to zero for the starting position
-            # TODO: integrate this in the drone-grid initialisation
-            idronegrid.grid.grid_multiplier[starting_y][starting_x] = 0
-            
-        self.dronegrid_properties = dronegrid_properties
-        
-    def _reset_drone(self, dronegrid_properties: list[DroneGridInfo])->None:
-        for idronegrid in dronegrid_properties:
-            idronegrid.drone.reset()
+        self._initialise_drones()
     
     def findpath(self, total_time)->DroneInfo:
         self._find_closest_circle()
@@ -78,14 +65,32 @@ class FindPathSwarm(FindPathGreedyTwoLayers):
         for drone_index, drone in enumerate(self.dronegrid_properties):
             # TODO: circle_drone_index was swapped around
             drone.circular_grid(grid=circular_grids[circle_drone_index[drone_index]])
+            
+    def _initialise_drones(self)->None:
+        '''
+            Initialise all drones and add the initial value to their total_path_value
+        '''
+        # === Initially move drones into 8 directions ===
+        for index, idronegrid in enumerate(self.dronegrid_properties):
+            # === Add value of starting point ===
+            current_drone_coords = idronegrid.drone.path[-1]
+
+            idronegrid.drone.add_to_sum(square_value=
+                                            idronegrid.grid.gridvalues[current_drone_coords[1]][current_drone_coords[0]])
+
     
     def _swarm_policy(self, total_time)->None:
         '''
             Move drone to either its assigned circular section or continue the algorithm
         '''
+        radii = divide_grid_circular(nr_drones=self.nr_drones, grid=self.dronegrid_properties[0].grid)
+
         for timestep in range(total_time):
+            # The grids need to be synced each cycle
+            # TODO: it should actually be during each run but it's horrible inefficient and for now i don't run into drones wanting to
+                    # occupy the same square
+            self._sync_grids()
             for drone_index, drone in enumerate(self.dronegrid_properties):
-                radii = divide_grid_circular(nr_drones=self.nr_drones, grid=drone.grid)
                 radius_index = self.circle_drone_index[drone_index]
 
                 drone_in_section, direction = drone.drone_direction(radii=radii, radius_index=radius_index)
@@ -95,7 +100,7 @@ class FindPathSwarm(FindPathGreedyTwoLayers):
                     self.dronegrid_properties[drone_index] = self._move_drone(drone=drone, timestep=timestep)
                 
                 else:
-                    # drone mus tbe moved to its assigned circular section
+                    # drone must be moved to its assigned circular section
                     pathvalues = drone.get_surrounding_values()
                     if direction==[1,0]:
                         # move drone upward
@@ -115,27 +120,47 @@ class FindPathSwarm(FindPathGreedyTwoLayers):
                         assert ValueError
                         print('ERROR: Direction not given!')
                     self.dronegrid_properties[drone_index] = self._update_dronegridinfo(pathvalues=pathvalues, drone=drone, timestep=timestep)
-                    
+    
+    def _sync_grids(self):
+        '''
+            Make sure all different grid have the same multiplier value
+            TODO: O(N*N*X) This function is spectacularly inefficient but idh time to improve it rn
+                    To improve this part introduce a shared grid_multiplier matrix. This requires rewriting all dataclasses though.
+        '''
+        grid_size = self.dronegrid_properties[0].grid.size
+        for index_row in range(grid_size[0]):
+            for index_col in range(grid_size[1]):
+                tmp = 1.1
+                for idronegrid in self.dronegrid_properties:
+                    tmp = min(idronegrid.grid.grid_multiplier[index_row, index_col], tmp)
+                
+                # all matrices get the minimum multiplier value
+                for dronegrid_index in range(len(self.dronegrid_properties)):
+                    self.dronegrid_properties[dronegrid_index].grid.grid_multiplier[index_row, index_col] = tmp
+
 class FindPathSwarmGif(FindPathSwarm):
     def _swarm_policy(self, total_time)->None:
         '''
             Move drone to either its assigned circular section or continue the algorithm
         '''
-        with open(os.path.join(BASE_DIR, 'gridinfo.json'), 'w') as file:
+        dronegriddict = {}
+        radii = divide_grid_circular(nr_drones=self.nr_drones, grid=self.dronegrid_properties[0].grid)
+
+        with open(os.path.join(BASE_DIR, 'data', 'gifs', 'dronegrid_data',  'gridinfo.json'), 'w') as file:
             for timestep in range(total_time):
-                for drone_index, drone in enumerate(self.dronegrid_properties):
-                    radii = divide_grid_circular(nr_drones=self.nr_drones, grid=drone.grid)
+                self._sync_grids()
+                for drone_index, dronegrid in enumerate(self.dronegrid_properties):
                     radius_index = self.circle_drone_index[drone_index]
 
-                    drone_in_section, direction = drone.drone_direction(radii=radii, radius_index=radius_index)
+                    drone_in_section, direction = dronegrid.drone_direction(radii=radii, radius_index=radius_index)
                     
                     if drone_in_section:
                         # perform standard two layer drone algo
-                        self.dronegrid_properties[drone_index] = self._move_drone(drone=drone, timestep=timestep)
+                        self.dronegrid_properties[drone_index] = self._move_drone(drone=dronegrid, timestep=timestep)
                     
                     else:
                         # drone mus tbe moved to its assigned circular section
-                        pathvalues = drone.get_surrounding_values()
+                        pathvalues = dronegrid.get_surrounding_values()
                         if direction==[1,0]:
                             # move drone upward
                             pathvalues[3:] = [MIN_VALUE]*len(pathvalues[3:])
@@ -153,10 +178,13 @@ class FindPathSwarmGif(FindPathSwarm):
                         else:
                             assert ValueError
                             print('ERROR: Direction not given!')
-                        self.dronegrid_properties[drone_index] = self._update_dronegridinfo(pathvalues=pathvalues, drone=drone, timestep=timestep)
+                        self.dronegrid_properties[drone_index] = self._update_dronegridinfo(pathvalues=pathvalues, drone=dronegrid, timestep=timestep)
                         
-                    # === Save grid info for each drone at each timestep ===
-                    dronegriddict = {}
-                    dronegriddict['grid'] = drone.grid.gridvalues.flatten()
-                    dronegriddict['drone_path'] = drone.drone.path
-                    dronegriddict['drone_path_values'] = drone.drone.total_path_value
+                # === Save grid info for each drone at each timestep ===
+                dronegriddict['grid_t'+str(timestep)] = dronegrid.grid.gridvalues.flatten().tolist()
+            
+            for drone_index, dronegrid in enumerate(self.dronegrid_properties):
+                dronegriddict['drone_path'+dronegrid.drone.name] = dronegrid.drone.path
+                dronegriddict['drone_path_values'+dronegrid.drone.name] = dronegrid.drone.total_path_value
+                
+            json.dump(dronegriddict, file)
--- src/tests/test_swarm.py
+++ src/tests/test_swarm.py
@@ -6,7 +6,7 @@ import copy
 
 # --- Internal ---
 from src.base import DroneInfo, GridInfo
-from src.algorithms.findpath_swarm_circular import FindPathSwarm
+from src.algorithms.findpath_swarm_circular import FindPathSwarm, FindPathSwarmGif
 from src.algorithms.utils.pathproperties import DroneGridInfo
 from src.tests.inputs import testinputs
 
@@ -21,16 +21,16 @@ class TestGrid(unittest.TestCase):
         total_time = 2 # this is a single timestep, it's assumed that timestep one is used to distribute the drones over the initial 8 squares.
         _, grid, drone, total_time = testinputs()
         dronegrid0 = DroneGridInfo( drone=DroneInfo(name='Drone0_section0', 
-                                                    starting_point=np.add([3, 3], 0)), 
+                                                    starting_point=[3, 3]), 
                                     grid=grid)
         dronegrid1 = DroneGridInfo( drone=DroneInfo(name='Drone1_section1', 
-                                                    starting_point=np.add([5, 6], 0)), 
+                                                    starting_point=[5, 6]), 
                                     grid=grid)
         dronegrid2 = DroneGridInfo( drone=DroneInfo(name='Drone2_section2', 
-                                                    starting_point=np.add([8, 10], 0)), 
+                                                    starting_point=[8, 10]), 
                                     grid=grid)
         dronegrid3 = DroneGridInfo( drone=DroneInfo(name='Drone3_section3', 
-                                                    starting_point=np.add([10,10], 0)), 
+                                                    starting_point=[10,10]), 
                                     grid=grid)
         
         # TODO: horrible coding convention but i ran into memory reference issues
@@ -39,7 +39,7 @@ class TestGrid(unittest.TestCase):
                             dronegrid2,
                             dronegrid3]
 
-        pathfinder = FindPathSwarm(dronegrid_properties=drone_properties)
+        pathfinder = FindPathSwarmGif(dronegrid_properties=drone_properties)
         pathfinder.findpath(total_time=total_time)
         
         # === Test whether correct circles were assigned to correct drones ===
